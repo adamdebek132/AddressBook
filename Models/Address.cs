42571e79e77b91828d6b8eec920aafc57cd31d50
kt <kt@DESKTOP-MG2JDKI>
2021-03-15 22:23:09 +0100

    update

---
+++ Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+
+        [Display(Name = "Twoja ulubiona ulica")]
+        [Required]
+        public string Street { get; set; }
+
+        [Display(Name = "Twoj ulubiony kod")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Twoje ulubione miasto")]
+        [Required]
+        public string City { get; set; }
+
+        [Display(Name = "Twoj ulubiony numer")]
+        [Required]
+        public int Number { get; set; }
+
+    }
+}
